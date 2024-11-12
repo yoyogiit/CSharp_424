f27257c1a00887582a619ea35b3b26f34eec29f6
yoyogiit <hsin4991@gmail.com>
2024-11-12 22:56:51 +0800

    新增 CSharp 作業

---
+++ Lab1App/Lab1App/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lab1App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // generates a secret number ranging between 0 and 99, inclusive
+            Random rng = new Random();
+            int SecretNum = rng.Next(0, 100);
+            int lower = 0, upper = 99;
+
+            // asks the player to guess a number.
+            Console.WriteLine("({0}, {1})?", lower, upper);
+            int guess = int.Parse(Console.ReadLine());
+
+            while (upper > lower)
+            {
+                if (guess == SecretNum)
+                {
+                    Console.WriteLine("Bingo!");
+                    break;
+                }
+                else if (guess < SecretNum)
+                {
+                    lower = guess + 1;
+                    Console.WriteLine("({0}, {1})?", lower, upper);
+                }
+                else
+                {
+                    upper = guess - 1;
+                    Console.WriteLine("({0}, {1})?", lower, upper);
+                }
+
+                if (lower == upper)
+                {
+                    Console.WriteLine("GG!");
+                    break;
+                }
+            }
+        }
+    }
+}
