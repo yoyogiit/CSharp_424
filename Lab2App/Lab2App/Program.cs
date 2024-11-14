c92271df5acba414f16e5bfa3f1c086e4ed7f91f
yoyogiit <hsin4991@gmail.com>
2024-11-14 21:11:38 +0800

    新增 CSharp 作業

---
+++ Lab2App/Lab2App/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Lab2App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of citizens:");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.Write("ID: ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+
+            Console.Write("Contactee: ");
+            int[] x = new int[N];
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                int j = random.Next(i, N);
+                int temp = x[i];
+                x[i] = x[j];
+                x[j] = temp;
+            }
+            foreach (int temp in x)
+            {
+                Console.Write("{0} ", temp);
+            }
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine("Enter id of infected citizen:");
+            int infected = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("These citizens are to be self-isolated in the following 14 days:");
+            int iso = x[0];
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == iso)
+                {
+                    iso = x[i];
+                }
+            }
+            Console.WriteLine("{0} ", iso);
+
+        }
+    }
+}
